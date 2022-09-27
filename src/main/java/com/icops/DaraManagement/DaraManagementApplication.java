9b5bab9f8b4afc790e6281da62945d6ec483fa21
A-Daffeh <adama.daffeh07@gmail.com>
2022-09-26 16:18:39 -0800

    Added the lombok and mysql driver

---
+++ src/main/java/com/icops/DaraManagement/DaraManagementApplication.java
@@ -0,0 +1,13 @@
+package com.icops.DaraManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaraManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaraManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icops/DaraManagement/DaraManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icops.DaraManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaraManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
